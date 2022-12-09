e24338bd159bfe9c8372990b59f7fe2646639156
Aurélien GARNIER <au2001sfg@gmail.com>
2022-12-09 17:56:39 +0100

    Clean build

--- dist/database/on-chain/entity/Address.d.ts
+++
@@ -1,8 +0,0 @@
-import { Collection } from "./Collection";
-export declare class Address {
-    id: number;
-    firstName: string;
-    lastName: string;
-    age: number;
-    collections: Collection[];
-}
