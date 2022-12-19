f0b06a6c459cd677ebf3eb3377e49d0ea1b92382
Aurélien GARNIER <au2001sfg@gmail.com>
2022-12-19 20:03:57 +0100

    Call TimescaleDB create_hypertable

--- dist/database/timescale/migration/1670944459545-init.js
+++ dist/database/timescale/migration/1670944459545-init.js
@@ -17,6 +17,7 @@ class init1670944459545 {
     up(queryRunner) {
         return __awaiter(this, void 0, void 0, function* () {
             yield queryRunner.query(`CREATE TABLE "transfers" ("txHash" character(64) NOT NULL, "logIdx" integer NOT NULL, "from" character(40) NOT NULL, "to" character(40) NOT NULL, "collection" character(40) NOT NULL, "item" numeric(78), "amount" numeric(78), "timestamp" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_dbae301f250e9d4435104066841" PRIMARY KEY ("txHash", "logIdx"))`);
+            yield queryRunner.query(`SELECT create_hypertable('transfers', 'timestamp');`);
         });
     }
     down(queryRunner) {
