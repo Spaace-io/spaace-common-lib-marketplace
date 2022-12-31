521ca51c04eae5ba93aec2a823c89b206035232a
Aurélien GARNIER <au2001sfg@gmail.com>
2022-12-31 16:32:47 +0100

    Install uuid contrib module

--- dist/database/off-chain/migration/1671029047358-addOrder.js
+++ dist/database/off-chain/migration/1671029047358-addOrder.js
@@ -16,6 +16,7 @@ class addOrder1671029047358 {
     }
     up(queryRunner) {
         return __awaiter(this, void 0, void 0, function* () {
+            yield queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
             yield queryRunner.query(`CREATE TABLE "order" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "item" character varying, "isAsk" boolean NOT NULL, "price" integer NOT NULL, "currency" character varying NOT NULL, "endTime" character varying NOT NULL, "startTime" character varying NOT NULL, "collection" character varying NOT NULL, "hash" character varying NOT NULL, "signer" character varying NOT NULL, "signature" character varying NOT NULL, "strategy" character varying NOT NULL, "tokenId" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL, "updated_at" TIMESTAMP NOT NULL, CONSTRAINT "PK_1031171c13130102495201e3e20" PRIMARY KEY ("id"))`);
         });
     }
