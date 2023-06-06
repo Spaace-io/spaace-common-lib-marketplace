82ece1293707ee732bee81066ba8818837063386
Jean-Vincent QUILICHINI <jv.quilichini@gmail.com>
2023-06-06 09:11:30 +0200

    feat: adding common LoginNonce entity

---
+++ src/database/off-chain/entity/LoginNonce.entity.ts
@@ -0,0 +1,23 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { User } from './User.entity';
+
+export const LOGIN_NONCE_VALID_PERIOD = 5 * 60 * 1000;
+
+@Entity()
+export class LoginNonce {
+  @PrimaryGeneratedColumn('uuid')
+  nonce!: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user', referencedColumnName: 'id' })
+  user!: User;
+
+  @Column({ default: () => 'CURRENT_TIMESTAMP' })
+  timestamp!: Date;
+}
