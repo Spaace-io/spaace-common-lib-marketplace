46b432919b220ab13cf4be11ba8cd5855202d1d0
Jean-Vincent QUILICHINI <jv.quilichini@gmail.com>
2023-06-07 17:54:39 +0200

    fix: fixing login_nonce migration

--- dist/database/off-chain/migration/1686153073505-addLoginNonce.js
+++ dist/database/off-chain/migration/1686153073505-addLoginNonce.js
@@ -16,8 +16,6 @@ class addLoginNonce1686153073505 {
     }
     up(queryRunner) {
         return __awaiter(this, void 0, void 0, function* () {
-            yield queryRunner.query(`CREATE TABLE "users" ("address" character(40) NOT NULL, CONSTRAINT "PK_b0ec0293d53a1385955f9834d5c" PRIMARY KEY ("address"))`);
-            yield queryRunner.query(`CREATE TABLE "orders" ("hash" character(64) NOT NULL, "user" character(40) NOT NULL, "collection" character varying NOT NULL, "tokenId" character varying, "isAsk" boolean NOT NULL, "price" numeric(78) NOT NULL, "currency" character varying NOT NULL, "startTime" TIMESTAMP NOT NULL, "endTime" TIMESTAMP NOT NULL, "signature" character varying NOT NULL, CONSTRAINT "PK_13ab9c024e81573c05451b9004f" PRIMARY KEY ("hash"))`);
             yield queryRunner.query(`CREATE TABLE "login_nonce" ("nonce" character(32) NOT NULL, "timestamp" TIMESTAMP NOT NULL DEFAULT now(), "user" character(40), CONSTRAINT "PK_b563be4d1127e83321bd33c9d16" PRIMARY KEY ("nonce"))`);
             yield queryRunner.query(`ALTER TABLE "login_nonce" ADD CONSTRAINT "FK_4137d2aae20dffff1f82a7284c6" FOREIGN KEY ("user") REFERENCES "users"("address") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         });
@@ -26,8 +24,6 @@ class addLoginNonce1686153073505 {
         return __awaiter(this, void 0, void 0, function* () {
             yield queryRunner.query(`ALTER TABLE "login_nonce" DROP CONSTRAINT "FK_4137d2aae20dffff1f82a7284c6"`);
             yield queryRunner.query(`DROP TABLE "login_nonce"`);
-            yield queryRunner.query(`DROP TABLE "orders"`);
-            yield queryRunner.query(`DROP TABLE "users"`);
         });
     }
 }
