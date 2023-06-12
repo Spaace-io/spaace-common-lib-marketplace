1286053a22ae50d6f17b3a90e8453933487400e8
Aurélien <git@garnier.dev>
2023-06-12 09:06:29 +0000

    Fix notable collections migration

--- dist/database/off-chain/migration/1686241295922-notableCollections.js
+++ dist/database/off-chain/migration/1686241295922-notableCollections.js
@@ -17,14 +17,12 @@ class notableCollections1686241295922 {
     up(queryRunner) {
         return __awaiter(this, void 0, void 0, function* () {
             yield queryRunner.query(`CREATE TABLE "notable_collections" ("collection" character(40) NOT NULL, CONSTRAINT "PK_5a76a7e64f51ddc26c6201ff047" PRIMARY KEY ("collection"))`);
-            yield queryRunner.query(`ALTER TABLE "orders" DROP COLUMN "collection"`);
-            yield queryRunner.query(`ALTER TABLE "orders" ADD "collection" character(40) NOT NULL`);
+            yield queryRunner.query(`ALTER TABLE "orders" ALTER COLUMN "collection" TYPE character(40)`);
         });
     }
     down(queryRunner) {
         return __awaiter(this, void 0, void 0, function* () {
-            yield queryRunner.query(`ALTER TABLE "orders" DROP COLUMN "collection"`);
-            yield queryRunner.query(`ALTER TABLE "orders" ADD "collection" character varying NOT NULL`);
+            yield queryRunner.query(`ALTER TABLE "orders" ALTER COLUMN "collection" TYPE character varying`);
             yield queryRunner.query(`DROP TABLE "notable_collections"`);
         });
     }
--- src/database/off-chain/migration/1686241295922-notableCollections.ts
+++ src/database/off-chain/migration/1686241295922-notableCollections.ts
@@ -7,16 +7,14 @@ export class notableCollections1686241295922 implements MigrationInterface {
     await queryRunner.query(
       `CREATE TABLE "notable_collections" ("collection" character(40) NOT NULL, CONSTRAINT "PK_5a76a7e64f51ddc26c6201ff047" PRIMARY KEY ("collection"))`,
     );
-    await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN "collection"`);
     await queryRunner.query(
-      `ALTER TABLE "orders" ADD "collection" character(40) NOT NULL`,
+      `ALTER TABLE "orders" ALTER COLUMN "collection" TYPE character(40)`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN "collection"`);
     await queryRunner.query(
-      `ALTER TABLE "orders" ADD "collection" character varying NOT NULL`,
+      `ALTER TABLE "orders" ALTER COLUMN "collection" TYPE character varying`,
     );
     await queryRunner.query(`DROP TABLE "notable_collections"`);
   }
