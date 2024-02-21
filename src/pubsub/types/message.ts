92f32df51b80b0c1d044dcc9adc666cb3554e75d
Muhammad Waleed <mujahidwaleed11@gmail.com>
2024-02-21 18:32:58 +0500

    arena pubsub message added

--- src/pubsub/types/message.ts
+++ src/pubsub/types/message.ts
@@ -1,6 +1,12 @@
-import { PubSubTopic, PubSubTrigger, PubSubData } from '.';
+import { PubSubTopic, PubSubTrigger, PubSubData, ArenaPubSubData } from '.';
+import { ArenaQuestTrigger } from '../../database';
 
 export interface PubSubMessage<T extends PubSubTrigger<PubSubTopic>> {
   trigger: T;
   data: PubSubData<T>;
+}
+
+export interface ArenaPubSubMessage {
+  trigger: ArenaQuestTrigger;
+  data: ArenaPubSubData<ArenaQuestTrigger>;
 }
