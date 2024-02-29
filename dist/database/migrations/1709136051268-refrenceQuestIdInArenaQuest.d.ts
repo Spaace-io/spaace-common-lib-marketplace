e50f9ecf51a1de4bf3782a1ba6f2d256d5acc3c7
Muhammad Waleed <mujahidwaleed11@gmail.com>
2024-02-29 11:56:10 +0500

    double migrations to add reference quest id removed

--- dist/database/migrations/1709136051268-refrenceQuestIdInArenaQuest.d.ts
+++
@@ -1,6 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-export declare class RefrenceQuestIdInArenaQuest1709136051268 implements MigrationInterface {
-    name: string;
-    up(queryRunner: QueryRunner): Promise<void>;
-    down(queryRunner: QueryRunner): Promise<void>;
-}
