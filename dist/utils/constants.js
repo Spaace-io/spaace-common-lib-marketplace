84d39cdd96be6a3cc2d65cf89938224196b025e2
sleepyqadir <a.qadir67@outlook.com>
2024-04-26 18:21:41 +0500

    added: weth address of sepolia

--- dist/utils/constants.js
+++ dist/utils/constants.js
@@ -4,10 +4,10 @@ exports.ETH_TOKENS = exports.WETH_ADDRESS = void 0;
 const ethers_1 = require("ethers");
 exports.WETH_ADDRESS = !process.env.TESTNET
     ? '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-    : '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6';
+    : '0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14';
 exports.ETH_TOKENS = [
     ethers_1.ethers.constants.AddressZero,
     '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-    '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6', // Goerli WETH9
+    '0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14', // sepolia WETH9
 ];
 //# sourceMappingURL=constants.js.map
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,10 +2,10 @@ import { ethers } from 'ethers';
 
 export const WETH_ADDRESS = !process.env.TESTNET
   ? '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-  : '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6';
+  : '0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14';
 
 export const ETH_TOKENS = [
   ethers.constants.AddressZero, // Native ETH
   '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // Mainnet WETH9
-  '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6', // Goerli WETH9
+  '0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14', // sepolia WETH9
 ];
