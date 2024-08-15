aa4745f063bf0b54fb6b4a6f032c34bad13bb1c4
NikosFotiadis <nikosfotiadis0@gmail.com>
2024-08-15 16:37:27 +0300

    fix; remove duplicate migration

--- dist/database/migrations/1719855230515-AddFulfillRemainingQuantities.d.ts
+++
@@ -1,6 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-export declare class AddFulfillRemainingQuantities1719855230515 implements MigrationInterface {
-    name: string;
-    up(queryRunner: QueryRunner): Promise<void>;
-    down(queryRunner: QueryRunner): Promise<void>;
-}
