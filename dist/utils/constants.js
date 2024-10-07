ce035bbb66f4d2190fe77c10ffdb69b090f8cf12
Antonis Poulakis <antwnic4@gmail.com>
2024-10-07 12:47:28 +0300

    feat: update sepolia WETH address to match the one used in opensea seaport

--- dist/utils/constants.js
+++ dist/utils/constants.js
@@ -4,10 +4,10 @@ exports.ETH_TOKENS = exports.WETH_ADDRESS = void 0;
 const ethers_1 = require("ethers");
 exports.WETH_ADDRESS = !process.env.TESTNET
     ? '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
-    : '0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14';
+    : '0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9';
 exports.ETH_TOKENS = [
     ethers_1.ethers.constants.AddressZero,
     '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-    '0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14', // sepolia WETH9
+    '0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9', // sepolia WETH9
 ];
 //# sourceMappingURL=constants.js.map
