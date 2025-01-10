73b398aa87b949eadd1d19b5b1ad2d42f332456d
Muhammad Ahmad <muhammad.ahmad8043@gmail.com>
2025-01-10 22:45:25 +0500

    feat: add prime field to collections_view

---
+++ dist/database/migrations/1736530711566-AddPrimeFieldToCollectionsView.d.ts
@@ -0,0 +1,6 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+export declare class AddPrimeFieldToCollectionsView1736530711566 implements MigrationInterface {
+    name: string;
+    up(queryRunner: QueryRunner): Promise<void>;
+    down(queryRunner: QueryRunner): Promise<void>;
+}
