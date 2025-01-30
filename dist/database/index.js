21f80d729f5838371c8be2676716780008756d56
Antonis Poulakis <antwnic4@gmail.com>
2025-01-30 07:32:34 +0200

    feat: add cluster config to Database

--- dist/database/index.js
+++ dist/database/index.js
@@ -13,7 +13,7 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
 var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
-var _a, _b, _c;
+var _a, _b, _c, _d, _e, _f, _g, _h;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Database = void 0;
 require("reflect-metadata");
@@ -25,8 +25,9 @@ const username = (_c = process.env.DATABASE_USERNAME) !== null && _c !== void 0 
 const password = process.env.DATABASE_PASSWORD;
 const database = process.env.DATABASE_NAME;
 const schema = process.env.DATABASE_SCHEMA;
+const applicationName = process.env.DATABASE_APPLICATION_NAME;
 const ssl = process.env.DATABASE_SSL === 'true';
-exports.Database = new typeorm_1.DataSource({
+const options = {
     type: 'postgres',
     host,
     port,
@@ -34,6 +35,7 @@ exports.Database = new typeorm_1.DataSource({
     password,
     database,
     schema,
+    applicationName,
     migrationsRun: false,
     logging: false,
     entities: [
@@ -45,7 +47,50 @@ exports.Database = new typeorm_1.DataSource({
     migrations: [__dirname + '/migrations/*-*.{js,ts}'],
     subscribers: [],
     ssl,
-});
+};
+const useCluster = (_d = process.env.DATABASE_USER_CLUSTER) !== null && _d !== void 0 ? _d : false;
+const masterHost = (_e = process.env.DATABASE_MASTER_HOST) !== null && _e !== void 0 ? _e : 'localhost';
+const masterPort = parseInt((_f = process.env.DATABASE_MASTER_PORT) !== null && _f !== void 0 ? _f : '5432', 10);
+const replicaHost = (_g = process.env.DATABASE_REPLICA_HOST) !== null && _g !== void 0 ? _g : 'localhost';
+const replicaPort = parseInt((_h = process.env.DATABASE_REPLICA_PORT) !== null && _h !== void 0 ? _h : '5432', 10);
+const replicationOptions = {
+    type: 'postgres',
+    schema,
+    migrationsRun: false,
+    logging: false,
+    entities: [
+        __dirname + '/tables/**.entity.{js,ts}',
+        __dirname + '/cache/**.entity.{js,ts}',
+        __dirname + '/types/**.view.{js,ts}',
+    ],
+    synchronize: false,
+    migrations: [__dirname + '/migrations/*-*.{js,ts}'],
+    subscribers: [],
+    replication: {
+        master: {
+            host: masterHost,
+            port: masterPort,
+            username,
+            password,
+            database,
+            applicationName,
+            ssl,
+        },
+        slaves: [
+            {
+                host: replicaHost,
+                port: replicaPort,
+                username,
+                password,
+                database,
+                applicationName,
+                ssl,
+            },
+        ],
+        defaultMode: 'slave',
+    },
+};
+exports.Database = new typeorm_1.DataSource(useCluster ? replicationOptions : options);
 __exportStar(require("./tables"), exports);
 __exportStar(require("./cache"), exports);
 __exportStar(require("./types"), exports);
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import '../config';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 const host = process.env.DATABASE_HOST ?? 'localhost';
 const port = parseInt(process.env.DATABASE_PORT ?? '5432', 10);
@@ -8,8 +9,10 @@ const username = process.env.DATABASE_USERNAME ?? 'root';
 const password = process.env.DATABASE_PASSWORD;
 const database = process.env.DATABASE_NAME;
 const schema = process.env.DATABASE_SCHEMA;
+const applicationName = process.env.DATABASE_APPLICATION_NAME;
 const ssl = process.env.DATABASE_SSL === 'true';
-export const Database = new DataSource({
+
+const options: DataSourceOptions = {
   type: 'postgres',
   host,
   port,
@@ -17,6 +20,7 @@ export const Database = new DataSource({
   password,
   database,
   schema,
+  applicationName,
   migrationsRun: false,
   logging: false,
   entities: [
@@ -28,7 +32,54 @@ export const Database = new DataSource({
   migrations: [__dirname + '/migrations/*-*.{js,ts}'],
   subscribers: [],
   ssl,
-});
+};
+
+const useCluster = process.env.DATABASE_USER_CLUSTER ?? false;
+const masterHost = process.env.DATABASE_MASTER_HOST ?? 'localhost';
+const masterPort = parseInt(process.env.DATABASE_MASTER_PORT ?? '5432', 10);
+const replicaHost = process.env.DATABASE_REPLICA_HOST ?? 'localhost';
+const replicaPort = parseInt(process.env.DATABASE_REPLICA_PORT ?? '5432', 10);
+
+const replicationOptions: PostgresConnectionOptions = {
+  type: 'postgres',
+  schema,
+  migrationsRun: false,
+  logging: false,
+  entities: [
+    __dirname + '/tables/**.entity.{js,ts}',
+    __dirname + '/cache/**.entity.{js,ts}',
+    __dirname + '/types/**.view.{js,ts}',
+  ],
+  synchronize: false,
+  migrations: [__dirname + '/migrations/*-*.{js,ts}'],
+  subscribers: [],
+  replication: {
+    master: {
+      host: masterHost,
+      port: masterPort,
+      username,
+      password,
+      database,
+      applicationName,
+      ssl,
+    },
+    slaves: [
+      {
+        host: replicaHost,
+        port: replicaPort,
+        username,
+        password,
+        database,
+        applicationName,
+        ssl,
+      },
+    ],
+    defaultMode: 'slave',
+  },
+};
+export const Database = new DataSource(
+  useCluster ? replicationOptions : options,
+);
 
 export * from './tables';
 export * from './cache';
