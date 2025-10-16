f7c4570395c569fd3b223a54ea6b8ce58865b809
ynikolenko <enikosoft@gmail.com>
2025-10-16 15:29:40 +0300

    Airdrop chapter 1

--- dist/database/migrations/1747798915413-collection_votes.d.ts
+++
@@ -1,6 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-export declare class CollectionVotes1747798915413 implements MigrationInterface {
-    name: string;
-    up(queryRunner: QueryRunner): Promise<void>;
-    down(queryRunner: QueryRunner): Promise<void>;
-}
