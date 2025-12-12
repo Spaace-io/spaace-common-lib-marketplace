59242348843fbd951ec9a43a7e1dd415f4eaa31e
ynikolenko <enikosoft@gmail.com>
2025-12-12 14:14:13 +0200

    Updated package with protected user fields

--- dist/database/tables/User.entity.js
+++ dist/database/tables/User.entity.js
@@ -22,6 +22,7 @@ let User = class User extends typeorm_1.BaseEntity {
 exports.User = User;
 __decorate([
     (0, graphql_1.Field)(() => String),
+    (0, class_transformer_1.Expose)(),
     (0, typeorm_1.PrimaryColumn)('char', { length: 40 }),
     (0, class_transformer_1.Transform)(({ value }) => ethers_1.ethers.utils.getAddress(value), {
         toPlainOnly: true,
@@ -31,11 +32,13 @@ __decorate([
 __decorate([
     (0, graphql_1.Field)(() => String, { nullable: true }),
     (0, typeorm_1.Column)('text', { nullable: true, unique: true }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", Object)
 ], User.prototype, "name", void 0);
 __decorate([
     (0, graphql_1.Field)(() => String, { nullable: true }),
     (0, typeorm_1.Column)('text', { nullable: true }),
+    (0, class_transformer_1.Expose)({ groups: ['me'] }),
     __metadata("design:type", Object)
 ], User.prototype, "email", void 0);
 __decorate([
@@ -43,11 +46,13 @@ __decorate([
         nullable: true,
     }),
     (0, typeorm_1.Column)({ type: 'varchar', length: 255, nullable: true }),
+    (0, class_transformer_1.Expose)({ groups: ['me'] }),
     __metadata("design:type", Object)
 ], User.prototype, "pendingEmail", void 0);
 __decorate([
     (0, graphql_1.Field)(() => EmailStatus_enum_1.EmailStatus),
     (0, typeorm_1.Column)({ type: 'enum', enum: EmailStatus_enum_1.EmailStatus, default: EmailStatus_enum_1.EmailStatus.UNSET }),
+    (0, class_transformer_1.Expose)({ groups: ['me'] }),
     __metadata("design:type", String)
 ], User.prototype, "emailStatus", void 0);
 __decorate([
@@ -55,43 +60,52 @@ __decorate([
         nullable: true,
     }),
     (0, typeorm_1.Column)({ type: 'timestamptz', nullable: true }),
+    (0, class_transformer_1.Expose)({ groups: ['me'] }),
     __metadata("design:type", Object)
 ], User.prototype, "emailVerifiedAt", void 0);
 __decorate([
     (0, typeorm_1.Column)({ type: 'varchar', length: 128, nullable: true }),
+    (0, class_transformer_1.Expose)({ groups: ['me'] }),
     __metadata("design:type", Object)
 ], User.prototype, "emailVerificationTokenHash", void 0);
 __decorate([
     (0, typeorm_1.Column)({ type: 'timestamptz', nullable: true }),
+    (0, class_transformer_1.Expose)({ groups: ['me'] }),
     __metadata("design:type", Object)
 ], User.prototype, "emailVerificationExpiresAt", void 0);
 __decorate([
     (0, typeorm_1.Column)({ type: 'timestamptz', nullable: true }),
+    (0, class_transformer_1.Expose)({ groups: ['me'] }),
     __metadata("design:type", Object)
 ], User.prototype, "emailVerificationLastSentAt", void 0);
 __decorate([
     (0, graphql_1.Field)(() => String, { nullable: true }),
     (0, typeorm_1.Column)('text', { nullable: true }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", Object)
 ], User.prototype, "biography", void 0);
 __decorate([
     (0, graphql_1.Field)(() => String, { nullable: true }),
     (0, typeorm_1.Column)('text', { nullable: true }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", Object)
 ], User.prototype, "imageUrl", void 0);
 __decorate([
     (0, graphql_1.Field)(() => String, { nullable: true }),
     (0, typeorm_1.Column)('text', { nullable: true }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", Object)
 ], User.prototype, "bannerUrl", void 0);
 __decorate([
     (0, graphql_1.Field)(() => Boolean),
     (0, typeorm_1.Column)('boolean', { default: false }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", Boolean)
 ], User.prototype, "admin", void 0);
 __decorate([
     (0, graphql_1.Field)(() => String),
     (0, typeorm_1.Column)('text', { unique: true }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", String)
 ], User.prototype, "referralCode", void 0);
 __decorate([
@@ -102,6 +116,7 @@ __decorate([
     (0, class_transformer_1.Transform)(({ value }) => (value !== null ? ethers_1.ethers.utils.getAddress(value) : null), {
         toPlainOnly: true,
     }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", Object)
 ], User.prototype, "referrerAddress", void 0);
 __decorate([
@@ -111,84 +126,101 @@ __decorate([
         enumName: 'access_level',
         default: AccessLevel_enum_1.AccessLevel.LOCKED,
     }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", String)
 ], User.prototype, "accessLevel", void 0);
 __decorate([
     (0, graphql_1.Field)(() => Date),
     (0, typeorm_1.Column)('timestamp without time zone', { default: () => 'CURRENT_TIMESTAMP' }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", Date)
 ], User.prototype, "timestamp", void 0);
 __decorate([
     (0, graphql_1.Field)(() => String, { nullable: true }),
     (0, typeorm_1.Column)('text', { nullable: true }),
+    (0, class_transformer_1.Expose)({ groups: ['me'] }),
     __metadata("design:type", Object)
 ], User.prototype, "twitterUsername", void 0);
 __decorate([
     (0, graphql_1.Field)(() => String, { nullable: true }),
     (0, typeorm_1.Column)('text', { nullable: true }),
+    (0, class_transformer_1.Expose)({ groups: ['me'] }),
     __metadata("design:type", Object)
 ], User.prototype, "twitterId", void 0);
 __decorate([
     (0, graphql_1.Field)(() => String, { nullable: true }),
     (0, typeorm_1.Column)('text', { nullable: true }),
+    (0, class_transformer_1.Expose)({ groups: ['me'] }),
     __metadata("design:type", Object)
 ], User.prototype, "twitterSecretToken", void 0);
 __decorate([
     (0, graphql_1.Field)(() => String, { nullable: true }),
     (0, typeorm_1.Column)('text', { nullable: true }),
+    (0, class_transformer_1.Expose)({ groups: ['me'] }),
     __metadata("design:type", Object)
 ], User.prototype, "twitterAccessToken", void 0);
 __decorate([
     (0, graphql_1.Field)(() => String, { nullable: true }),
     (0, typeorm_1.Column)('text', { nullable: true }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", Object)
 ], User.prototype, "sharedAirdropOGImage", void 0);
 __decorate([
     (0, graphql_1.Field)(() => String, { nullable: true }),
     (0, typeorm_1.Column)('text', { nullable: true }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", Object)
 ], User.prototype, "sharedReferralImage", void 0);
 __decorate([
     (0, graphql_1.Field)(() => Boolean),
     (0, typeorm_1.Column)('boolean', { default: false }),
+    (0, class_transformer_1.Expose)({ groups: ['me'] }),
     __metadata("design:type", Boolean)
 ], User.prototype, "checkedAirdropChapter0", void 0);
 __decorate([
     (0, graphql_1.Field)(() => Boolean),
     (0, typeorm_1.Column)('boolean', { default: false }),
+    (0, class_transformer_1.Expose)({ groups: ['me'] }),
     __metadata("design:type", Boolean)
 ], User.prototype, "checkedAirdropChapter1", void 0);
 __decorate([
     (0, graphql_1.Field)(() => Boolean),
     (0, typeorm_1.Column)('boolean', { default: false }),
+    (0, class_transformer_1.Expose)({ groups: ['me'] }),
     __metadata("design:type", Boolean)
 ], User.prototype, "checkedAirdropOpenseaChapter1", void 0);
 __decorate([
     (0, graphql_1.Field)(() => String, { nullable: true }),
     (0, typeorm_1.Column)('text', { nullable: true }),
+    (0, class_transformer_1.Expose)({ groups: ['me'] }),
     __metadata("design:type", Object)
 ], User.prototype, "discordId", void 0);
 __decorate([
     (0, graphql_1.Field)(() => String, { nullable: true }),
     (0, typeorm_1.Column)('text', { nullable: true }),
+    (0, class_transformer_1.Expose)({ groups: ['me'] }),
     __metadata("design:type", Object)
 ], User.prototype, "discordUsername", void 0);
 __decorate([
     (0, typeorm_1.Column)('text', { nullable: true }),
+    (0, class_transformer_1.Expose)({ groups: ['me'] }),
     __metadata("design:type", Object)
 ], User.prototype, "discordAccessToken", void 0);
 __decorate([
     (0, typeorm_1.Column)('text', { nullable: true }),
+    (0, class_transformer_1.Expose)({ groups: ['me'] }),
     __metadata("design:type", Object)
 ], User.prototype, "discordRefreshToken", void 0);
 __decorate([
     (0, graphql_1.Field)(() => Boolean),
     (0, typeorm_1.Column)({ type: 'boolean', default: false }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", Boolean)
 ], User.prototype, "isAmbassador", void 0);
 __decorate([
     (0, graphql_1.Field)(() => String, { nullable: true }),
     (0, typeorm_1.Column)('varchar', { length: 32, nullable: true }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", Object)
 ], User.prototype, "referralStatus", void 0);
 __decorate([
@@ -199,49 +231,59 @@ __decorate([
         enumName: 'users_status_enum',
         default: UserStatus_enum_1.UserStatus.ACTIVE,
     }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", String)
 ], User.prototype, "status", void 0);
 __decorate([
     (0, graphql_1.Field)(() => Number, { nullable: true }),
     (0, typeorm_1.Column)('integer', { nullable: true }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", Object)
 ], User.prototype, "abuseScore", void 0);
 __decorate([
     (0, graphql_1.Field)(() => String, { nullable: true }),
     (0, typeorm_1.Column)('text', { nullable: true }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", Object)
 ], User.prototype, "abuseReason", void 0);
 __decorate([
     (0, graphql_1.Field)(() => Boolean),
     (0, typeorm_1.Column)('boolean', { default: false }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", Boolean)
 ], User.prototype, "checkedAbuseReport", void 0);
 __decorate([
     (0, graphql_1.Field)(() => Date),
     (0, typeorm_1.Column)({ type: 'timestamptz', default: () => 'NOW()' }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", Date)
 ], User.prototype, "statusUpdatedAt", void 0);
 __decorate([
     (0, graphql_1.Field)(() => String, { nullable: true }),
     (0, typeorm_1.Column)('varchar', { length: 45, nullable: true }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", Object)
 ], User.prototype, "creationIP", void 0);
 __decorate([
     (0, graphql_1.Field)(() => String, { nullable: true }),
     (0, typeorm_1.Column)('varchar', { length: 100, nullable: true }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", Object)
 ], User.prototype, "creationFingerprint", void 0);
 __decorate([
     (0, graphql_1.Field)(() => String, { nullable: true }),
     (0, typeorm_1.Column)('varchar', { length: 45, nullable: true }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", Object)
 ], User.prototype, "lastConnectionIP", void 0);
 __decorate([
     (0, graphql_1.Field)(() => Date, { nullable: true }),
     (0, typeorm_1.Column)({ type: 'timestamptz', nullable: true }),
+    (0, class_transformer_1.Expose)(),
     __metadata("design:type", Object)
 ], User.prototype, "lastConnectionAt", void 0);
 exports.User = User = __decorate([
+    (0, class_transformer_1.Exclude)(),
     (0, graphql_1.ObjectType)(),
     (0, typeorm_1.Entity)({ name: 'users' })
 ], User);
--- src/database/tables/User.entity.ts
+++ src/database/tables/User.entity.ts
@@ -1,5 +1,5 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import { Transform } from 'class-transformer';
+import { Exclude, Expose, Transform } from 'class-transformer';
 import {
   Entity,
   PrimaryColumn,
@@ -14,10 +14,12 @@ import { AccessLevel } from '../enums/AccessLevel.enum';
 import { EmailStatus } from '../enums/EmailStatus.enum';
 import { UserStatus } from '../enums/UserStatus.enum';
 
+@Exclude()
 @ObjectType()
 @Entity({ name: 'users' })
 export class User extends BaseEntity {
   @Field(() => String)
+  @Expose()
   @PrimaryColumn('char', { length: 40 })
   @Transform(({ value }) => ethers.utils.getAddress(value), {
     toPlainOnly: true,
@@ -26,55 +28,68 @@ export class User extends BaseEntity {
 
   @Field(() => String, { nullable: true })
   @Column('text', { nullable: true, unique: true })
+  @Expose()
   name!: string | null;
 
   @Field(() => String, { nullable: true })
   @Column('text', { nullable: true })
+  @Expose({ groups: ['me'] })
   email!: string | null;
 
   @Field(() => String, {
     nullable: true,
   })
   @Column({ type: 'varchar', length: 255, nullable: true })
+  @Expose({ groups: ['me'] })
   pendingEmail: string | null;
 
   @Field(() => EmailStatus)
   @Column({ type: 'enum', enum: EmailStatus, default: EmailStatus.UNSET })
+  @Expose({ groups: ['me'] })
   emailStatus: EmailStatus;
 
   @Field(() => Date, {
     nullable: true,
   })
   @Column({ type: 'timestamptz', nullable: true })
+  @Expose({ groups: ['me'] })
   emailVerifiedAt: Date | null;
 
   @Column({ type: 'varchar', length: 128, nullable: true })
+  @Expose({ groups: ['me'] })
   emailVerificationTokenHash: string | null;
 
   @Column({ type: 'timestamptz', nullable: true })
+  @Expose({ groups: ['me'] })
   emailVerificationExpiresAt: Date | null;
 
   @Column({ type: 'timestamptz', nullable: true })
+  @Expose({ groups: ['me'] })
   emailVerificationLastSentAt: Date | null;
 
   @Field(() => String, { nullable: true })
   @Column('text', { nullable: true })
+  @Expose()
   biography!: string | null;
 
   @Field(() => String, { nullable: true })
   @Column('text', { nullable: true })
+  @Expose()
   imageUrl!: string | null;
 
   @Field(() => String, { nullable: true })
   @Column('text', { nullable: true })
+  @Expose()
   bannerUrl!: string | null;
 
   @Field(() => Boolean)
   @Column('boolean', { default: false })
+  @Expose()
   admin!: boolean;
 
   @Field(() => String)
   @Column('text', { unique: true })
+  @Expose()
   referralCode!: string;
 
   @Field(() => String, { nullable: true })
@@ -87,6 +102,7 @@ export class User extends BaseEntity {
       toPlainOnly: true,
     },
   )
+  @Expose()
   referrerAddress!: string | null;
 
   @Field(() => AccessLevel)
@@ -95,68 +111,85 @@ export class User extends BaseEntity {
     enumName: 'access_level',
     default: AccessLevel.LOCKED,
   })
+  @Expose()
   accessLevel!: AccessLevel;
 
   @Field(() => Date)
   @Column('timestamp without time zone', { default: () => 'CURRENT_TIMESTAMP' })
+  @Expose()
   timestamp!: Date;
 
   @Field(() => String, { nullable: true })
   @Column('text', { nullable: true })
+  @Expose({ groups: ['me'] })
   twitterUsername!: string | null;
 
   @Field(() => String, { nullable: true })
   @Column('text', { nullable: true })
+  @Expose({ groups: ['me'] })
   twitterId!: string | null;
 
   @Field(() => String, { nullable: true })
   @Column('text', { nullable: true })
+  @Expose({ groups: ['me'] })
   twitterSecretToken!: string | null;
 
   @Field(() => String, { nullable: true })
   @Column('text', { nullable: true })
+  @Expose({ groups: ['me'] })
   twitterAccessToken!: string | null;
 
   @Field(() => String, { nullable: true })
   @Column('text', { nullable: true })
+  @Expose()
   sharedAirdropOGImage!: string | null;
 
   @Field(() => String, { nullable: true })
   @Column('text', { nullable: true })
+  @Expose()
   sharedReferralImage!: string | null;
 
   @Field(() => Boolean)
   @Column('boolean', { default: false })
+  @Expose({ groups: ['me'] })
   checkedAirdropChapter0!: boolean;
 
   @Field(() => Boolean)
   @Column('boolean', { default: false })
+  @Expose({ groups: ['me'] })
   checkedAirdropChapter1!: boolean;
 
   @Field(() => Boolean)
   @Column('boolean', { default: false })
+  @Expose({ groups: ['me'] })
   checkedAirdropOpenseaChapter1!: boolean;
 
   @Field(() => String, { nullable: true })
   @Column('text', { nullable: true })
+  @Expose({ groups: ['me'] })
   discordId!: string | null;
 
   @Field(() => String, { nullable: true })
   @Column('text', { nullable: true })
+  @Expose({ groups: ['me'] })
   discordUsername!: string | null;
 
   @Column('text', { nullable: true })
+  @Expose({ groups: ['me'] })
   discordAccessToken!: string | null;
 
   @Column('text', { nullable: true })
+  @Expose({ groups: ['me'] })
   discordRefreshToken!: string | null;
 
   @Field(() => Boolean)
   @Column({ type: 'boolean', default: false })
+  @Expose()
   isAmbassador!: boolean;
 
   @Field(() => String, { nullable: true })
   @Column('varchar', { length: 32, nullable: true })
+  @Expose()
   referralStatus!: 'pending' | 'active' | null;
 
   @Field(() => UserStatus)
@@ -166,38 +199,47 @@ export class User extends BaseEntity {
     enumName: 'users_status_enum',
     default: UserStatus.ACTIVE,
   })
+  @Expose()
   status!: UserStatus;
 
   @Field(() => Number, { nullable: true })
   @Column('integer', { nullable: true })
+  @Expose()
   abuseScore!: number | null;
 
   @Field(() => String, { nullable: true })
   @Column('text', { nullable: true })
+  @Expose()
   abuseReason!: string | null;
 
   @Field(() => Boolean)
   @Column('boolean', { default: false })
+  @Expose()
   checkedAbuseReport!: boolean;
 
   @Field(() => Date)
   @Column({ type: 'timestamptz', default: () => 'NOW()' })
+  @Expose()
   statusUpdatedAt!: Date;
 
   // Anti-bot detection fields
   @Field(() => String, { nullable: true })
   @Column('varchar', { length: 45, nullable: true })
+  @Expose()
   creationIP!: string | null;
 
   @Field(() => String, { nullable: true })
   @Column('varchar', { length: 100, nullable: true })
+  @Expose()
   creationFingerprint!: string | null;
 
   @Field(() => String, { nullable: true })
   @Column('varchar', { length: 45, nullable: true })
+  @Expose()
   lastConnectionIP!: string | null;
 
   @Field(() => Date, { nullable: true })
   @Column({ type: 'timestamptz', nullable: true })
+  @Expose()
   lastConnectionAt!: Date | null;
 }
